b12f5d5a4e138b27ec43ed91c9f7cd7e791379e4
Danilo Correia <60863302+dacs30@users.noreply.github.com>
2022-12-22 18:22:28 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,98 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  DatePicker,
+  IDatePickerStyles,
+  ITextProps,
+  defaultDatePickerStrings,
+  IDatePickerStrings,
+  PrimaryButton,
+} from '@fluentui/react';
+import { useConst } from '@fluentui/react-hooks';
+import { addDays } from '@fluentui/react/lib/DateTimeUtilities';
 
-function App() {
+const datePickerStyles: Partial<IDatePickerStyles> = { root: { maxWidth: 300, marginTop: 15 } };
+
+function App(props: any) {
+  const [maxDate, setMaxDate] = useState<Date | undefined>(undefined);
+  const [minDate, setMinDate] = useState<Date | undefined>(undefined);
+  const [daysBetweenDates, setDaysBetweenDates] = useState<number | undefined>(undefined);
+
+
+  const setMaxDateHandler = (date: Date | null | undefined) => {
+    // set max date to date minus 1 day
+    date && setMaxDate(addDays(date, -1));
+  };
+
+  const setMinDateHandler = (date: Date | null | undefined) => {
+    // set min date to date plus 1 day
+    date && setMinDate(addDays(date, 1));
+  };
+
+  // calculate the number of days between the two dates
+  const daysBetween = (date1: Date, date2: Date) => {
+    const diffTime = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
+  };
+
+  const resetCount = () => {
+    setMaxDate(undefined);
+    setMinDate(undefined);
+    setDaysBetweenDates(undefined);
+  };
+
+  const strings: IDatePickerStrings = useConst(() => ({
+    ...defaultDatePickerStrings,
+    isOutOfBoundsErrorMessage: `Dates must be one after the other`,
+  }));
+
+  useEffect(() => {
+    if (maxDate && minDate) {
+      setDaysBetweenDates(daysBetween(addDays(maxDate, 1), addDays(minDate, -1)));
+    }
+  }, [maxDate, minDate]);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Days counter v2 📅</h1>
+        <div className='subtitle'>
+          <p>
+            A simple app to count the number of days between two dates. The first date must be before the second date.
+          </p>
+        </div>
+        <DatePicker
+          styles={datePickerStyles}
+          // DatePicker uses English strings by default. For localized apps, you must override this prop.
+          placeholder="Select a date..."
+          value={minDate ? addDays(minDate, -1) : undefined}
+          ariaLabel="Select a date"
+          maxDate={maxDate ?? undefined}
+          onSelectDate={setMinDateHandler}
+          allowTextInput
+          strings={strings}
+        />
+        <DatePicker
+          styles={datePickerStyles}
+          // DatePicker uses English strings by default. For localized apps, you must override this prop.
+          placeholder="Select a date..."
+          ariaLabel="Select a date"
+          value={maxDate ? addDays(maxDate, 1) : undefined}
+          minDate={minDate}
+          allowTextInput
+          onSelectDate={setMaxDateHandler}
+          strings={strings}
+        />
+        {/* fluent ui button */}
+        <div>
+          <h4>
+            {daysBetweenDates ? `There are ${daysBetweenDates} days between the two dates` : 0}
+          </h4>
+        </div>
+        <PrimaryButton onClick={resetCount} text="Reset" allowDisabledFocus />
       </header>
     </div>
   );
